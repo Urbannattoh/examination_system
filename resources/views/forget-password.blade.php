bdbb909220329f305666349c1f1e56109266cb58
urbannattoh <kiokokyalo@gmail.com>
2023-06-05 20:58:45 +0300

    better look login

--- resources/views/forget-password.blade.php
+++ resources/views/forget-password.blade.php
@@ -1,29 +1,62 @@
-@extends('layout/layout-common')
+<!doctype html>
+<html lang="en">
 
-@section('space-work')
-<h1>Forget Password</h1>
+<head>
+    <title>PTI ONLINE EXAMINATION</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-@if($errors->any())
-@foreach($errors->all() as $error)
-<p style="color:red;">{{ $error }}</p>
-@endforeach
-@endif
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
 
-@if(Session::has('error'))
-    <p style="color:red;">{{ Session::get('error')}} </p>
-@endif
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
-@if(Session::has('success'))
-    <p style="color:green;">{{ Session::get('success')}} </p>
-@endif
+    <link rel="stylesheet" href="css/stylelogin.css">
 
-<form action="{{ route('forgetPassword')}}" method="POST">
-    @csrf
-    <input type="email" name="email" placeholder="Enter Email">
-    <br><br>
-    <input type="submit" value="Forget Password">
+</head>
 
-</form>
-<a href="/">Login </a>
+<body class="img js-fullheight" style="background-image: url(images/bg.jpg);">
+    <section class="ftco-section">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-6 text-center mb-5">
+                    <h2 class="heading-section">ONLINE EXAMINATION SYSTEM</h2>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-md-6 col-lg-4">
+                    <div class="login-wrap p-0">
+                        <h3 class="mb-4 text-center">Forgot your Password?</h3>
+                        <form action="{{ route('forgetPassword')}}" method="POST">
+                           
+                            @csrf  
+                            <input type="email" class="form-control" name="email" placeholder="Enter Email">
+                            <br><br>
+                                                    
+                            <div class="form-group">
+                                <button type="submit" class="form-control btn btn-primary submit px-3">Forget Password</button>
+                            </div>
+                        </form>
+                        <p class="w-100 text-center">&mdash; Or Sign In With &mdash;</p>
+                        <div class="social d-flex text-center">
+                                
+                            <a href="/login" class="px-2 py-2 mr-md-1 rounded">
+                                Login</a>
+                            <a href="#" class="px-2 py-2 mr-md-1 rounded"><span class="ion-logo-facebook mr-2"></span>
+                                Facebook</a>
+                            <a href="#" class="px-2 py-2 ml-md-1 rounded"><span class="ion-logo-twitter mr-2"></span>
+                                Twitter</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
-@endsection
+    <script src="js/jquery.min.js"></script>
+    <script src="js/popper.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+
+</body>
+
+</html>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,28 +1,79 @@
-@extends('layout/layout-common')
+<!doctype html>
+<html lang="en">
 
-@section('space-work')
-<h1>Login</h1>
+<head>
+    <title>PROSECUTION TRAINING INSTITUTE</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-@if($errors->any())
-    @foreach($errors->all() as $error)
-        <p style="color:red;">{{ $error }}</p>
-    @endforeach
-@endif
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
 
-@if(Session::has('error'))
-    <p style="color:red;">{{ Session::get('error')}} </p>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
-@endif
+    <link rel="stylesheet" href="css/stylelogin.css">
 
-<form action="{{ route('userLogin')}}" method="POST">
-    @csrf
-    <input type="email" name="email" placeholder="Enter Email">
-    <br><br>
-    <input type="password" name="password" placeholder="Enter Password">
-    <br><br>
-    <input type="submit" value="Login">
+</head>
 
-</form>
-<a href="/forget-password">Forget Password</a>
+<body class="img js-fullheight" style="background-image: url(images/bg.jpg);">
+    <section class="ftco-section">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-6 text-center mb-5">
+                    <h2 class="heading-section">EXAMINATION SYSTEM</h2>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-md-6 col-lg-4">
+                    <div class="login-wrap p-0">
+                        <h3 class="mb-4 text-center">Have an account?</h3>
 
-@endsection
+                        <form action="{{ route('userLogin')}}" method="POST">
+                            @csrf
+
+                            <div class="form-group">
+                                <input type="email" class="form-control" name="email" placeholder="Enter Email">
+                                
+                            </div>
+                            <div class="form-group">
+
+                                <input type="password" class="form-control" name="password" placeholder="Enter Password"
+                                    required>
+                                <span toggle="#password-field"
+                                    class="fa fa-fw fa-eye field-icon toggle-password"></span>
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" class="form-control btn btn-primary submit px-3">Sign In</button>
+                            </div>
+                            <div class="form-group d-md-flex">
+                                <div class="w-50">
+                                    <label class="checkbox-wrap checkbox-primary">Remember Me
+                                        <input type="checkbox" checked>
+                                        <span class="checkmark"></span>
+                                    </label>
+                                </div>
+                                <div class="w-50 text-md-right">
+                                    <a href="#" style="color: #fff">Forgot Password</a>
+                                </div>
+                            </div>
+                        </form>
+                        <p class="w-100 text-center">&mdash; Or Sign In With &mdash;</p>
+                        <div class="social d-flex text-center">
+                            <a href="#" class="px-2 py-2 mr-md-1 rounded"><span class="ion-logo-facebook mr-2"></span>
+                                Facebook</a>
+                            <a href="#" class="px-2 py-2 ml-md-1 rounded"><span class="ion-logo-twitter mr-2"></span>
+                                Twitter</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/popper.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+
+</body>
+
+</html>
